dafea5baf183e987e8cf243c99f1c4be37b2c18c
IGN-Styly <claudioresendeptbr@gmail.com>
2023-04-18 18:54:34 +0100

    Yay fix

--- commands/islinked.js
+++ commands/islinked.js
@@ -12,19 +12,17 @@ async function IsLink(tag) {
 }
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("link")
-    .setDescription(
-      "Provides information about the user if linked with minecraft"
-    )
-    .addUserOption((option) =>
-      option.setName("ign").setDescription("Their discord").setRequired(true)
-    ),
-
+   data: new SlashCommandBuilder()
+  .setName("link")
+  .setDescription(
+    "Provides information about the user if linked with minecraft"
+  )
+  .addUserOption((option) => option.setName("ign").setDescription("Their discord").setRequired(true)
+  ),
   async execute(interaction) {
-    const usr = await IsLink(interaction.options.getUser("ign").tag);
-    const fail = new EmbedBuilder(
-      {
+  const usr = await IsLink(interaction.options.getUser("ign").tag);
+  const fail = new EmbedBuilder(
+    {
       "title": "Verification Status",
       "color": 15961000,
       "description": `[${interaction.options.getUser("ign").tag}] Isn't Verified`,
@@ -37,47 +35,46 @@ module.exports = {
       "footer": {},
       "fields": []
     }
-    )
+  );
 
-    const link = new EmbedBuilder({
-      "title": "How to Link My account",
-      "color": 7440858,
-      "description": "1. Type \"/profile\" in the in-game chat and press enter\n2. Find the icon called \"Social Media\"\n3. Find the icon called \"Discord\"\n4. Go to the Discord app and click on your name on the bottom left to copy your Discord tag\n5. Go back in game and paste that copied tag in the chat\n6. If a book pops up, click \"I understand\"",
-      "timestamp": "",
-      "url": "https://www.youtube.com/watch?v=gqUPbkxxKLI",
-      "author": {},
-      "image": {},
-      "thumbnail": {
-        "url": "https://hypixel.net/styles/hypixel-v2/images/header-logo.png"
-      },
-      "footer": {},
-      "fields": []
-    });
-    if (await usr === undefined || usr.length ==0) {
-      // await interaction.reply(`User [${interaction.options.getUser("ign")}] Does not have an Account Linked`);
-      await interaction.reply({
-        embeds: [fail,link],
-      });
-      return;
-    }
-
-    const success = new EmbedBuilder({
-      "title": "Verification Status",
-      "color": 10937249,
-      "description": `[${interaction.options.getUser("ign").tag}] Is verified and owns [${usr[0].user}]`,
-      "timestamp": "",
-      "author": {
-        "name": "Necron"
-      },
-      "image": {},
-      "thumbnail": {},
-      "footer": {},
-      "fields": []
-    })
+  const link = new EmbedBuilder({
+    "title": "How to Link My account",
+    "color": 7440858,
+    "description": "1. Type \"/profile\" in the in-game chat and press enter\n2. Find the icon called \"Social Media\"\n3. Find the icon called \"Discord\"\n4. Go to the Discord app and click on your name on the bottom left to copy your Discord tag\n5. Go back in game and paste that copied tag in the chat\n6. If a book pops up, click \"I understand\"",
+    "timestamp": "",
+    "url": "https://www.youtube.com/watch?v=gqUPbkxxKLI",
+    "author": {},
+    "image": {},
+    "thumbnail": {
+      "url": "https://hypixel.net/styles/hypixel-v2/images/header-logo.png"
+    },
+    "footer": {},
+    "fields": []
+  });
+  if (await usr === undefined || usr.length == 0) {
+    // await interaction.reply(`User [${interaction.options.getUser("ign")}] Does not have an Account Linked`);
     await interaction.reply({
-      embeds: [success],
+      embeds: [fail, link],
     });
-    return
-    // await interaction.reply(`User [${interaction.options.getUser("ign")}] owns the account with the IGN [${usr.user}]`);
-  },
-};
+    return;
+  }
+
+  const success = new EmbedBuilder({
+    "title": "Verification Status",
+    "color": 10937249,
+    "description": `[${interaction.options.getUser("ign").tag}] Is verified and owns [${usr[0].user}]`,
+    "timestamp": "",
+    "author": {
+      "name": "Necron"
+    },
+    "image": {},
+    "thumbnail": {},
+    "footer": {},
+    "fields": []
+  });
+  await interaction.reply({
+    embeds: [success],
+  });
+  return;
+  // await interaction.reply(`User [${interaction.options.getUser("ign")}] owns the account with the IGN [${usr.user}]`);
+}}
--- commands/verify.js
+++ commands/verify.js
@@ -1,32 +1,23 @@
-const {
-  SlashCommandBuilder,
-  Modal,
-  EmbedBuilder,
-  TextInputComponent,
-  TextInputBuilder,
-  TextInputStyle,
-  ActionRowBuilder
-} = require("discord.js");
+const { SlashCommandBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } =require("discord.js");
 const { Events, ModalBuilder } = require("discord.js");
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("verify")
-    .setDescription("Links the discord tag to the Minecraft IGN"),
+module.exports ={ 
+  data:new SlashCommandBuilder()
+  .setName("verify")
+  .setDescription("Links the discord tag to the Minecraft IGN"),
   async execute(interaction) {
-    const modal = new ModalBuilder()
-      .setCustomId("verify")
-      .setTitle("Verify");
+  const modal = new ModalBuilder()
+    .setCustomId("verify")
+    .setTitle("Verify")
 
-    const favoriteColorInput = new TextInputBuilder()
-      .setCustomId("ign")
-      // The label is the prompt the user sees for this input
-      .setLabel("What's your Minecraft IGN?")
-      // Short means only a single line of text
-      .setStyle(TextInputStyle.Short)
-      .setRequired(true);
-      const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
-      modal.addComponents(firstActionRow);
-      await interaction.showModal(modal);
-  },
-};
+  const favoriteColorInput = new TextInputBuilder()
+    .setCustomId("ign")
+    // The label is the prompt the user sees for this input
+    .setLabel("What's your Minecraft IGN?")
+    // Short means only a single line of text
+    .setStyle(TextInputStyle.Short)
+    .setRequired(true);
+  const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
+  modal.addComponents(firstActionRow);
+  await interaction.showModal(modal);
+}}
